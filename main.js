9695bd3a78c403ce8d4a88595ed98ac5c336430b
Sreeejit <77166984+Sreeejit@users.noreply.github.com>
2021-10-11 15:27:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // https://teachablemachine.withgoogle.com/models/QHO0ZEX9U/
+Bow = 0;
+Meow = 0;
+Background = 0;
+
 function startClassification()
 {
     console.log("startClassification")
@@ -13,6 +17,19 @@ function modelReady(){
     classifier.classify(gotResults);
 }
 
-function gotResults(){
+function gotResults(error,results){
  console.log("gotResults!")
+ if(error){
+    console.log(error);
+ }
+ else{
+console.log(results);
+// r = floor (random(1,255));
+// g = floor(random(1,255));
+// b = floor(random(1,255));
+final_result = results[0].label;
+document.getElementById('HearingIsNeeded').innerHTML = "I can hear - " + final_result;
+
+ 
+ }
 }
